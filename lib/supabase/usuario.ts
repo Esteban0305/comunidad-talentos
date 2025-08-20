879725e5fd30a2c2ccf5604a9d9cfdae90154828
= <=>
2025-08-20 12:32:47 -0600

    feat: LogIn with Supabase Auth

--- lib/supabase/usuario.ts
+++ lib/supabase/usuario.ts
@@ -4,8 +4,23 @@ import { supabaseServer } from './server';
 export async function registrarUsuarioSupabase(correo: string, contrasena: string) {
   const { data, error } = await supabaseServer.auth.signUp({
     email: correo,
-    password: contrasena,
-    phone: '312'
+    password: contrasena
+  });
+  if (error) throw new Error(error.message);
+
+  // if (data.user) {
+  //   await supabaseServer
+  //     .from('usuarios')
+  //     .insert({ id_usuario: data.user.id, telefono });
+  // }
+
+  return data;
+}
+
+export async function iniciarSesionUsuarioSupabase(correo: string, contrasena: string) {
+  const { data, error } = await supabaseServer.auth.signInWithPassword({
+    email: correo,
+    password: contrasena
   });
   if (error) throw new Error(error.message);
   return data;
