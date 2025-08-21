9554dacda3fdfc7fd6853c09681e0369ad7d65ee
= <=>
2025-08-20 20:10:51 -0600

    feat: Link to register and login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <div>Portal en desarrollo</div>
+      <Link href="/login">Ir a la página de autenticación</Link>
+      <br />
+      <Link href="/registro">Ir a la página de registro</Link>
     </main>
   );
 }
